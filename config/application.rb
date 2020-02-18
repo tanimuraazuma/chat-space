cd9ed68ac87d650364d15459954b74feb1b0327a
tanimuraazuma <azumax3252@yahoo.co.jp>
2020-02-19 01:54:18 +0900

    add_ajax_to_message

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+config.time_zone = 'Tokyo'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
